0334304798f685df2565b844f7cfe3cb7356e9fd
laps98 <pine998@hotmail.com>
2024-04-24 00:00:42 -0300

    Perfect

--- lib/components/level.dart
+++ lib/components/level.dart
@@ -9,9 +9,8 @@ class Level extends World {
   final String levelName;
   final Player player;
   late TiledComponent level;
-  List<CollisionBlock> collisionBlocks = [];
-
   Level({required this.levelName, required this.player});
+  List<CollisionBlock> collisionBlocks = [];
 
   @override
   FutureOr<void> onLoad() async {
@@ -19,6 +18,53 @@ class Level extends World {
 
     add(level);
 
+    _spawningObjects();
+    _addCollisions();
+    // final spawnPointsLayer = level.tileMap.getLayer<ObjectGroup>('Spawnpoints');
+    //
+    // if (spawnPointsLayer != null) {
+    //   for (final spawnPoint in spawnPointsLayer.objects) {
+    //     switch (spawnPoint.class_) {
+    //       case 'Player':
+    //         player.position = Vector2(spawnPoint.x, spawnPoint.y);
+    //         player.scale.x = 1;
+    //         add(player);
+    //         break;
+    //       default:
+    //     }
+    //   }
+    //
+    //   final collisionsLayer = level.tileMap.getLayer<ObjectGroup>('Collisions');
+    //
+    //   if (collisionsLayer != null) {
+    //     for (final collision in collisionsLayer.objects) {
+    //       switch (collision.class_) {
+    //         case 'Platform':
+    //           final platform = CollisionBlock(
+    //             position: Vector2(collision.x, collision.y),
+    //             size: Vector2(collision.width, collision.height),
+    //             isPlatform: true,
+    //           );
+    //           collisionBlocks.add(platform);
+    //           add(platform);
+    //           break;
+    //         default:
+    //           final block = CollisionBlock(
+    //             position: Vector2(collision.x, collision.y),
+    //             size: Vector2(collision.width, collision.height),
+    //           );
+    //           collisionBlocks.add(block);
+    //           add(block);
+    //       }
+    //     }
+    //   }
+    //
+    //   player.collisionBlocks = collisionBlocks;
+    // }
+      return super.onLoad();
+  }
+
+  void _spawningObjects() {
     final spawnPointsLayer = level.tileMap.getLayer<ObjectGroup>('Spawnpoints');
 
     if (spawnPointsLayer != null) {
@@ -29,37 +75,76 @@ class Level extends World {
             player.scale.x = 1;
             add(player);
             break;
+          // case 'Fruit':
+          //   final fruit = Fruit(
+          //     fruit: spawnPoint.name,
+          //     position: Vector2(spawnPoint.x, spawnPoint.y),
+          //     size: Vector2(spawnPoint.width, spawnPoint.height),
+          //   );
+          //   add(fruit);
+          //   break;
+          // case 'Saw':
+          //   final isVertical = spawnPoint.properties.getValue('isVertical');
+          //   final offNeg = spawnPoint.properties.getValue('offNeg');
+          //   final offPos = spawnPoint.properties.getValue('offPos');
+          //   final saw = Saw(
+          //     isVertical: isVertical,
+          //     offNeg: offNeg,
+          //     offPos: offPos,
+          //     position: Vector2(spawnPoint.x, spawnPoint.y),
+          //     size: Vector2(spawnPoint.width, spawnPoint.height),
+          //   );
+          //   add(saw);
+          //   break;
+          // case 'Checkpoint':
+          //   final checkpoint = Checkpoint(
+          //     position: Vector2(spawnPoint.x, spawnPoint.y),
+          //     size: Vector2(spawnPoint.width, spawnPoint.height),
+          //   );
+          //   add(checkpoint);
+          //   break;
+          // case 'Chicken':
+          //   final offNeg = spawnPoint.properties.getValue('offNeg');
+          //   final offPos = spawnPoint.properties.getValue('offPos');
+          //   final chicken = Chicken(
+          //     position: Vector2(spawnPoint.x, spawnPoint.y),
+          //     size: Vector2(spawnPoint.width, spawnPoint.height),
+          //     offNeg: offNeg,
+          //     offPos: offPos,
+          //   );
+          //   add(chicken);
+          //   break;
           default:
         }
       }
+    }
+  }
 
-      final collisionsLayer = level.tileMap.getLayer<ObjectGroup>('Collisions');
+  void _addCollisions() {
+    final collisionsLayer = level.tileMap.getLayer<ObjectGroup>('Collisions');
 
-      if (collisionsLayer != null) {
-        for (final collision in collisionsLayer.objects) {
-          switch (collision.class_) {
-            case 'Platform':
-              final platform = CollisionBlock(
-                position: Vector2(collision.x, collision.y),
-                size: Vector2(collision.width, collision.height),
-                isPlatform: true,
-              );
-              collisionBlocks.add(platform);
-              add(platform);
-              break;
-            default:
-              final block = CollisionBlock(
-                position: Vector2(collision.x, collision.y),
-                size: Vector2(collision.width, collision.height),
-              );
-              collisionBlocks.add(block);
-              add(block);
-          }
+    if (collisionsLayer != null) {
+      for (final collision in collisionsLayer.objects) {
+        switch (collision.class_) {
+          case 'Platform':
+            final platform = CollisionBlock(
+              position: Vector2(collision.x, collision.y),
+              size: Vector2(collision.width, collision.height),
+              isPlatform: true,
+            );
+            collisionBlocks.add(platform);
+            add(platform);
+            break;
+          default:
+            final block = CollisionBlock(
+              position: Vector2(collision.x, collision.y),
+              size: Vector2(collision.width, collision.height),
+            );
+            collisionBlocks.add(block);
+            add(block);
         }
       }
-
-      player.collisionBlocks = collisionBlocks;
-      return super.onLoad();
     }
+    player.collisionBlocks = collisionBlocks;
   }
 }
--- lib/components/player.dart
+++ lib/components/player.dart
@@ -1,12 +1,14 @@
 import 'dart:async';
 
+import 'package:flame/collisions.dart';
 import 'package:flame/components.dart';
 import 'package:flutter/services.dart';
 import 'package:game/components/collision_block.dart';
+import 'package:game/components/custom_hitbox.dart';
 import 'package:game/components/utils.dart';
 import 'package:game/pixel_adventude.dart';
 
-enum PlayerState { idle, running, jumping }
+enum PlayerState { idle, running, jumping, falling }
 
 class Player extends SpriteAnimationGroupComponent
     with HasGameRef<PixelAdventure>, KeyboardHandler {
@@ -21,6 +23,7 @@ class Player extends SpriteAnimationGroupComponent
   late final SpriteAnimation idleAnimation;
   late final SpriteAnimation runningAnimation;
   late final SpriteAnimation jumpingAnimation;
+  late final SpriteAnimation fallingAnimation;
 
   final double _gravity = 9.8;
   final double _jumpForce = 260;
@@ -32,22 +35,33 @@ class Player extends SpriteAnimationGroupComponent
   bool isOnGround = false;
   bool hasJumped = false;
   List<CollisionBlock> collisionBlocks = [];
+  CustomHitbox hitbox = CustomHitbox(
+    offsetX: 10,
+    offsetY: 4,
+    width: 14,
+    height: 28,
+  );
 
   @override
   FutureOr<void> onLoad() {
     _loadAllAnimations();
-    debugMode = true;
+    // debugMode = true;
+
     startingPosition = Vector2(position.x, position.y);
+
+    add(RectangleHitbox(
+      position: Vector2(hitbox.offsetX, hitbox.offsetY),
+      size: Vector2(hitbox.width, hitbox.height),
+    ));
     return super.onLoad();
   }
-
   @override
   void update(double dt) {
     _updatePlayerState();
     _updatePlayerMovement(dt);
+    _checkHorizontalCollisions();
     _applyGravity(dt);
     _checkVerticalCollisions();
-    _checkHorizontalCollisions();
     super.update(dt);
   }
 
@@ -71,12 +85,14 @@ class Player extends SpriteAnimationGroupComponent
     idleAnimation = _spriteAnimator('Idle', 11);
     runningAnimation = _spriteAnimator('Run', 12);
     jumpingAnimation = _spriteAnimator('Jump', 1);
+    fallingAnimation = _spriteAnimator('Fall', 1);
 
     //List of all animations
     animations = {
       PlayerState.idle: idleAnimation,
       PlayerState.running: runningAnimation,
       PlayerState.jumping: jumpingAnimation,
+      PlayerState.falling: fallingAnimation,
     };
     // Set current animation
     current = PlayerState.idle;
@@ -106,7 +122,7 @@ class Player extends SpriteAnimationGroupComponent
     if (velocity.x > 0 || velocity.x < 0) playerState = PlayerState.running;
 
     // check if Falling set to falling
-    //if (velocity.y > 0) playerState = PlayerState.falling;
+    if (velocity.y > 0) playerState = PlayerState.falling;
 
     // Checks if jumping, set to jumping
     if (velocity.y < 0) playerState = PlayerState.jumping;
@@ -137,17 +153,19 @@ class Player extends SpriteAnimationGroupComponent
         if (checkCollision(this, block)) {
           if (velocity.x > 0) {
             velocity.x = 0;
-            position.x = block.x - width;
+            position.x = block.x - hitbox.offsetX - hitbox.width;
+            break;
           }
           if (velocity.x < 0) {
             velocity.x = 0;
-            position.x = block.x + block.width + width;
+            position.x = block.x + block.width + hitbox.width + hitbox.offsetX;
             break;
           }
         }
       }
     }
   }
+
 
   void _applyGravity(double dt) {
     velocity.y += _gravity;
@@ -161,7 +179,7 @@ class Player extends SpriteAnimationGroupComponent
         if (checkCollision(this, block)) {
           if (velocity.y > 0) {
             velocity.y = 0;
-            position.y = block.y - width;
+            position.y = block.y - hitbox.height - hitbox.offsetY;
             isOnGround = true;
             break;
           }
@@ -170,13 +188,13 @@ class Player extends SpriteAnimationGroupComponent
         if (checkCollision(this, block)) {
           if (velocity.y > 0) {
             velocity.y = 0;
-            position.y = block.y - width;
+            position.y = block.y - hitbox.height - hitbox.offsetY;
             isOnGround = true;
             break;
           }
           if (velocity.y < 0) {
             velocity.y = 0;
-            position.y = block.y + block.height;
+            position.y = block.y + block.height - hitbox.offsetY;
           }
         }
       }
